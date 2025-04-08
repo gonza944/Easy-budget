a4aa483f303995fe1b0d99e336a21647e20bd46e
Gonzalo Ariza <gonza94.4@gmail.com>
2025-04-08 08:36:46 -0300

    Update package dependencies and enhance global styles with Tailwind CSS and new color variables. Integrate custom fonts for improved typography in layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Rubik, Titan_One } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import '@/app/globals.css'
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const fontSans = Rubik({
+	subsets: ['latin'],
+	variable: '--font-sans',
+})
+
+const fontHeadings = Titan_One({
+	subsets: ['latin'],
+	variable: '--font-headings',
+	weight: '400',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,13 +24,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			{/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+			<body
+				className={`${fontSans.variable} ${fontHeadings.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	)
 }
